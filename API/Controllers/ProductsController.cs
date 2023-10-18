887fdb19700be78048c6ab0b108cb29f354dfd49
anweshaswain-mcd <anwesha.swain@capgemini.com>
2023-10-18 15:26:37 +0530

    API Basics

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
+using Core.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -6,16 +9,36 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
         {
-            return "this will be a list of products";
+            _context = context;
+        }
+
+        //[HttpGet]----------1st
+        // public string GetProducts()
+        // {
+        //     return "this will be a list of products";
+        // }
+
+        // [HttpGet]-------------2nd
+        // public ActionResult<List<Product>> GetProducts()
+        // {
+        //     var products = _context.Products.ToList();
+        //     return products;
+        // }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return "this will be a product";
+            return await _context.Products.FindAsync(id);   
         }
     }
 }
