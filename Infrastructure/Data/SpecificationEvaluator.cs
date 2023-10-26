1677dd09abe2604593645c5283636b310f65e0b3
anweshaswain-mcd <anwesha.swain@capgemini.com>
2023-10-26 16:38:21 +0530

    API Paging, Filtering, Sorting, Searching.

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Core.Entities;
 using Core.Specifications;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,22 @@ namespace Infrastructure.Data
             {
                 query = query.Where(spec.Criteria);   //exp- p => p.productTypeId == id
             }
+
+            if(spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy);   
+            }
+
+            if(spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);   
+            }
+
+            if(spec.IsPagingEnabled)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take);
+            }
+
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
             return query;
         }
